c1fc445471ab0447546bf1a8a0c51ef1579c3e54
RBennyjay <ejohn1@byupathway.edu>
2025-03-09 20:12:46 +0100

    Updated variable naming to camel case

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,14 +6,14 @@ class Program
     {   
         // Ask the user for their name
         Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         Console.WriteLine();
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
 
     }
 }
