d53f9252dcaf5bec91371710c6e4788193afa521
RBennyjay <ejohn1@byupathway.edu>
2025-03-09 19:06:33 +0100

    Finished Exercise1 Project

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,17 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    {   
+        // Ask the user for their name
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
     }
 }
