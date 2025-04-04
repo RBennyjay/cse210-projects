e6a691b4af1cd46594cc8e570b41f92a66d06801
RBennyjay <ejohn1@byupathway.edu>
2025-04-04 16:51:17 +0100

    Finished W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test Assignment
+        Assignment assignment = new Assignment("Ebenezer John", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Dannie Botha", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Sylvia Jacobs", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
+
