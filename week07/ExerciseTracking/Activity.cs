a5d37a16f8beff07c67c4181dc3dd8d86168c9d9
RBennyjay <ejohn1@byupathway.edu>
2025-04-16 06:36:06 +0100

    finished - W07 Assignment: Exercise Tracking Program (Foundation #3)

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime date;
+    private int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public DateTime GetDate() => date;
+    public int GetMinutes() => minutes;
+
+    public abstract double GetDistance(); // km
+    public abstract double GetSpeed();    // kph
+    public abstract double GetPace();     // min per km
+
+    public virtual string GetSummary()
+    {
+        return $"{date:dd MMM yyyy} {this.GetType().Name} ({minutes} min): " +
+               $"Distance: {GetDistance():0.0} km, " +
+               $"Speed: {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
+}
